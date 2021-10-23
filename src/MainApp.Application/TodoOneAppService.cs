7bf4f6a78b0e1fb9f3807b854114bb2398db18aa
Anto Subash <antosubash@live.com>
2021-10-23 14:26:59 +0200

    new module

---
+++ src/MainApp.Application/TodoOneAppService.cs
@@ -0,0 +1,50 @@
+﻿using ModuleA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace MainApp
+{
+    public class TodoOneAppService : MainAppAppService
+    {
+        private readonly IRepository<TodoOne, Guid> todoOneRepository;
+
+        public TodoOneAppService(IRepository<TodoOne, Guid> todoOneRepository)
+        {
+            this.todoOneRepository = todoOneRepository;
+        }
+
+        public async Task<List<TodoOneDto>> GetAll()
+        {
+            return ObjectMapper.Map<List<TodoOne>, List<TodoOneDto>>(await todoOneRepository.GetListAsync());
+        }
+
+        public async Task<TodoOneDto> CreateAsync(TodoOneDto todoOneDto)
+        {
+            var TodoOne = ObjectMapper.Map<TodoOneDto, TodoOne>(todoOneDto);
+            var createdTodoOne = await todoOneRepository.InsertAsync(TodoOne);
+            return ObjectMapper.Map<TodoOne, TodoOneDto>(createdTodoOne);
+        }
+
+        public async Task<TodoOneDto> UpdateAsync(TodoOneDto todoOneDto)
+        {
+            var TodoOne = ObjectMapper.Map<TodoOneDto, TodoOne>(todoOneDto);
+            var createdTodoOne = await todoOneRepository.UpdateAsync(TodoOne);
+            return ObjectMapper.Map<TodoOne, TodoOneDto>(createdTodoOne);
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var TodoOne = await todoOneRepository.FirstOrDefaultAsync(x => x.Id == id);
+            if (TodoOne != null)
+            {
+                await todoOneRepository.DeleteAsync(TodoOne);
+                return true;
+            }
+            return false;
+        }
+    }
+}
